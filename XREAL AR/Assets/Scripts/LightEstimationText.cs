86d47a9e1b662d622fb6ab0c7adeaba6e56c6f55
Meongjeong <258xsw@naver.com>
2024-10-06 23:28:52 +0900

    Light Estimation으로 현실공간의 광원정보를 받아 실시간으로 값을 출력

---
+++ XREAL AR/Assets/Scripts/LightEstimationText.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.XR.ARFoundation;
+
+public class LightEstimationText : MonoBehaviour
+{
+    [SerializeField]
+    private ARCameraManager arCameraManager;
+
+    [SerializeField]
+    private Text brightnessValue;
+
+    [SerializeField]
+    private Text colorCorrectionValue;
+
+    private Light currentLight;
+
+    private void Awake()
+    {
+        currentLight = GetComponent<Light>();
+    }
+
+    private void OnEnable()
+    {
+        arCameraManager.frameReceived += FrameUpdated;
+    }
+
+    private void OnDisable()
+    {
+        arCameraManager.frameReceived -= FrameUpdated;
+    }
+    private void FrameUpdated(ARCameraFrameEventArgs args)
+    {
+        if (args.lightEstimation.averageBrightness.HasValue) 
+        {
+            brightnessValue.text = $"Brightness: {args.lightEstimation.averageBrightness.Value}";
+            currentLight.intensity = args.lightEstimation.averageBrightness.Value;
+        }
+
+        if (args.lightEstimation.colorCorrection.HasValue) 
+        {
+            colorCorrectionValue.text = $"Color: {args.lightEstimation.colorCorrection.Value}";
+            currentLight.color = args.lightEstimation.colorCorrection.Value;
+        }
+    }
+}
